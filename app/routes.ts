f56e1a3fb873a9d19b2c9b95ecee87904c00a9f7
Chikezie Simon <simonchikezie44@gmail.com>
2025-08-28 14:08:43 +0100

    Add blog functionality with dynamic post loading and error handling; implement NotFound page

--- app/routes.ts
+++ app/routes.ts
@@ -8,5 +8,6 @@ export default [
     route('blog', './routes/blog/index.tsx'),
     route('projects', './routes/projects/index.tsx'),
     route('projects/:id', './routes/projects/details.tsx'),
+    route('*', './routes/errors/not-found.tsx'),
   ]),
 ] satisfies RouteConfig;
--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -1,8 +1,25 @@
-const BlogPage = () => {
+import { URL } from 'url';
+import type { Route } from '../blog/+types/index';
+import type { PostMeta } from '~/types';
+import PostCard from '~/components/post-card';
+export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
+  const url = new URL('/posts-meta.json', request.url);
+  console.log(url);
+  const res = await fetch(url.href);
+  if (!res.ok) throw new Error('Failed to fetch posts');
+  const data = await res.json();
+  return { posts: data };
+}
+const BlogPage = ({ loaderData }: Route.ComponentProps) => {
+  const { posts } = loaderData as { posts: PostMeta[] };
+
   return (
-    <>
-      <h2 className="text-white font-bold mt-3">Blog Page</h2>
-    </>
+    <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
+      <h2 className="text-3xl font-bold mb-8 text-white">📝 Blog</h2>
+      {posts.map((post: PostMeta) => (
+        <PostCard key={post.slug} post={post} />
+      ))}
+    </div>
   );
 };
 
