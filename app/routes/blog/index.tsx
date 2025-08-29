e9dca6d863e67bdbe40cb4f7b3a9c26c23e514e8
Chikezie Simon <simonchikezie44@gmail.com>
2025-08-29 17:40:07 +0100

    filtered posts

--- app/routes/blog/index.tsx
+++ app/routes/blog/index.tsx
@@ -4,6 +4,7 @@ import type { PostMeta } from '~/types';
 import PostCard from '~/components/post-card';
 import Pagination from '~/components/pagination';
 import { useState } from 'react';
+import PostFilter from '~/components/post-filter';
 
 export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: PostMeta[] }> {
   const url = new URL('/posts-meta.json', request.url);
@@ -18,16 +19,33 @@ export async function loader({ request }: Route.LoaderArgs): Promise<{ posts: Po
 }
 const BlogPage = ({ loaderData }: Route.ComponentProps) => {
   const [currentPage, setCurrentPage] = useState(1);
+  const [searchQuery, setSearchQuery] = useState('');
   const { posts } = loaderData as { posts: PostMeta[] };
   const postPerPage = 3;
-  const totalPages = Math.ceil(posts.length / postPerPage);
+
+  const filteredPosts = posts.filter((post) => {
+    const query = searchQuery?.toLocaleLowerCase();
+    return (
+      post.title.toLocaleLowerCase().includes(query) ||
+      post.excerpt.toLocaleLowerCase().includes(query)
+    );
+  });
+
   const lastIndex = currentPage * postPerPage;
   const firstIndex = lastIndex - postPerPage;
-  const currentPosts = posts.slice(firstIndex, lastIndex);
-
+  const currentPosts = filteredPosts.slice(firstIndex, lastIndex);
+  const totalPages = Math.ceil(filteredPosts.length / postPerPage);
+  
   return (
     <div className="max-w-3xl mx-auto mt-10 px-6 py-6 bg-gray-900">
       <h2 className="text-3xl font-bold mb-8 text-white">📝 Blog</h2>
+      <PostFilter
+        searchQuery={searchQuery}
+        onSearchChange={(searchQuery) => {
+          setSearchQuery(searchQuery);
+          setCurrentPage(1);
+        }}
+      />
       {currentPosts.map((post: PostMeta) => (
         <PostCard key={post.slug} post={post} />
       ))}
